78a333ecd0366f50f078bc35b5b808793d1ad337
vincenzo <vincenzodnm@outlook.it>
2022-04-05 00:15:53 +0200

    fix

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/packege.json'));
+app.use(express.static('./dist/package.json'));
 app.get("/*", function(req, res) {
-  res.sendFile('index.html', {root: 'dist/packaje.json/'}
+  res.sendFile('index.html', {root: 'dist/package.json/'}
 );
 });
 app.listen(process.env.PORT || 8080);
